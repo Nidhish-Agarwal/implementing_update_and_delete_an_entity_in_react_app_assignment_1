40bf3d8af835febb78d6683e2867bd17f6bb6772
Nidhish-Agarwal <nidhishagarwal14@gmail.com>
2025-02-03 10:17:40 +0530

    Completed the assingment

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
+import { useEffect } from "react";
 import UpdateItem from "./components/UpdateItem";
+import axios from "axios";
 
 // use the following link to get the data
 // `/doors` will give you all the doors, to get a specific door use `/doors/1`.
 const API_URI = `http://${import.meta.env.VITE_API_URI}/doors`;
 
 function App() {
+  console.log(API_URI);
+  let data;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${API_URI}/1`);
+        console.log(response.data);
+        data = response.data;
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   // Get the existing item from the server
   // const [item, setItem] = useState(null);
   // pass the item to UpdateItem as a prop
 
-  return <UpdateItem />;
+  return <UpdateItem item={data} />;
 }
 
 export default App;
--- src/components/UpdateItem.jsx
+++ src/components/UpdateItem.jsx
@@ -1,11 +1,59 @@
-const UpdateItem = ({ item }) => {
-    // 1. Create a state for the form
-    // 2. Create a function to handle the form submission
-    // 3. Create a function to handle the form input changes
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-    // your code here
-    return null;
+const UpdateItem = ({ item }) => {
+  const API_URI = `http://${import.meta.env.VITE_API_URI}/doors`;
+
+  const [formData, setFormData] = useState({
+    name: item.name || "",
+    description: item.description || "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const response = await axios.put(`${API_URI}/${item.id}`, formData);
+      console.log(response);
+    } catch (er) {
+      console.log(er);
+    }
+  };
+  // 1. Create a state for the form
+  // 2. Create a function to handle the form submission
+  // 3. Create a function to handle the form input changes
+
+  // your code here
+  return (
+    <div>
+      <form action="" onSubmit={handleSubmit}>
+        <label htmlFor="">Name : </label>
+        <input
+          type="text"
+          name="name"
+          id=""
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <label htmlFor="">Description : </label>
+        <input
+          type="text"
+          name="description"
+          id=""
+          value={formData.description}
+          onChange={handleChange}
+        />
+        <button type="submit">Update</button>
+      </form>
+    </div>
+  );
 };
 
 export default UpdateItem;
-
